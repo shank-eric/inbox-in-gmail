05c67a919207ba0ea6665fa1fdbcd10453068cf8
Eric Shank <eric@everfi.com>
2023-08-15 15:35:34 -0400

    fix(outlook): consolidate topBar selector
    - appears to be the same on office365 as office.com

--- src/js/outlook/index.js
+++ src/js/outlook/index.js
@@ -4,8 +4,7 @@ import keyboard from './keyboard.js';
 
 async function setupOutlookNavigation() {
   const buttonContainer = document.querySelector('.V2uYM');
-  const topBarSelector = window.location.host === 'outlook.office365.com' ? '.ms-CommandBar' : '#paddleContainer';
-  const topBar = await observeForElement(document, topBarSelector);
+  const topBar = await observeForElement(document, '#paddleContainer');
   topBar.appendChild(buttonContainer);
   addClass(topBar, 'top-bar');
 }
